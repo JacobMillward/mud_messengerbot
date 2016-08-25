1c792e576d02a75fa8bb2c85e457db5e529cea5d
Jacob Millward <jacob.millward@gmail.com>
2016-08-25 21:25:20 +0100

    Forced input commands to lowercase

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/webhook/', function (req, res) {
         if (event.message && event.message.text) {
             let text = event.message.text;
             let tokens = parse.tokenise(text);
-            let proc = parse.getCommandProc(tokens[0]);
+            let proc = parse.getCommandProc(tokens[0].toLowerCase());
             let responseText = proc(tokens);
             sendTextMessage(sender, responseText);
         }
