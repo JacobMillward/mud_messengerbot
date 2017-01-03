4e5555cd7d758dcf569cf77596a939330a417f6d
Jacob Millward <jacob.millward@gmail.com>
2017-01-03 22:00:01 +0000

    Add usage strings to commands

--- commands.js
+++ commands.js
@@ -3,18 +3,20 @@
 var commands = [
   {
     name: "help",
+    usage: "help",
     description: "Show this list of all possible commands",
     proc: function(player, targets){
       let output = "";
       var arrayLength = commands.length;
       for(var i = 0; i < arrayLength; i++) {
-        output += commands[i].name + " - " + commands[i].description + "\n";
+        output +=`${commands[i].name} - ${commands[i].description}\nUsage: ${commands[i].usage}\n\n`;
       }
-      return output;
+      return output.trim();
     }
   },
   {
     name: "test",
+    usage: "test param_1 ... param_n",
     description: "Prints out a list of all passed in arguments",
     proc: function(player, targets) {
       let response = "";
